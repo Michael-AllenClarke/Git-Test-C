b451d35a80718cf9b9a875b4d6e82095cea683c1
Michael-AllenClarke <clarkemichaelallen07@gmail.com>
2025-02-20 11:56:00 -0500

    remove 45

--- program.py
+++ program.py
@@ -3,7 +3,7 @@ letters = ['A', 'B', 'C', 'D']
 for letter in letters:
     print(letter.lower())
 
-numbers = ['1','2','3','45']
+numbers = ['1','2','3','4']
 
 for number in numbers:
     print(number)
