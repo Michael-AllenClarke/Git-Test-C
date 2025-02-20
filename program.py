a2faaec9c26f08f83a7bd76e3fa95318f413667c
Michael-AllenClarke <miclarke@packer.edu>
2025-02-20 11:45:53 -0500

    Update program.py
    fa fa fa

--- program.py
+++ program.py
@@ -7,3 +7,19 @@ numbers = ['1','2','3','4']
 
 for number in numbers:
     print(number)
+
+timer = 30
+import time
+
+while timer > 0:
+    timer -= 1
+    if timer < 30 and timer > 1:
+        print('There is ' + str(timer) + " seconds left..." )
+        time.sleep(1)
+    if timer == 1:
+        print('There is ' + str(timer) + " second left..." )
+        time.sleep(1)
+    if timer == 0:
+        print('Resetting...')
+        time.sleep(3)
+        timer = 30 
